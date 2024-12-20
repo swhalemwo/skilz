72f2245e596f21dc0b0660597bd7e0d30510630c
swhalemwo <jojo.ae@hotmail.de>
2024-12-20 16:49:36 +0100

    basic setup of finding OA ids in place

--- oa_authors.el
+++ oa_authors.el
@@ -3,9 +3,25 @@
 (require 'helm)             ; For using Helm
 
 
-;; (defun skillz-dl-author-search (search-term)
-;;   (with-temp-buffer
-(setq xx (json-parse-string (plz 'get "https://api.openalex.org/authors?search=olav%20velthuis")))
+;; (setq xx (json-parse-string (plz 'get "https://api.openalex.org/authors?search=patrick%20brown")))
+
+;; (setq results (gethash "results" xx))
+
+;; (setq res1 (aref results 0))
+;; (get-hashtable-keys res1)
+
+;; (setq affs (gethash "affiliations" res1))
+
+;; (setq aff0 (aref affs 0))
+
+;; (get-hashtable-keys aff0)
+
+;; (mapcar (lambda (x) (gethash "display_name" (gethash "institution" x))) affs)
+
+;; (setq ins0 (gethash "institution" aff0))
+
+;; (get-hashtable-keys ins0)
+;; (gethash "display_name" ins0)
 
 (defun get-hashtable-keys (hashtable)
   "Return a list of keys from HASHTABLE."
@@ -43,31 +59,30 @@
 	  ;; generate helm candidates: pseudo multiple column support by padding entries
 	  (oa-candidates
 	    (mapcar (lambda (search-result)
-		      (cons (mapconcat (lambda (key)
-					 (string-pad (format "%s" (gethash (car key) search-result)) (cdr key)))
-			      skillz-values-to-extract) ;; each search result gets reduced to one line
+		      (cons (concat
+			      (mapconcat (lambda (key)
+					   (string-pad (format "%s" (gethash (car key) search-result)) (cdr key)))
+				skillz-values-to-extract) ;; each search result gets reduced to one line
+			      ;; (mapconcat (lambda (x) (gethash "display_name" (gethash "institution"
+			      (mapconcat (lambda (x) (gethash "display_name" (gethash "institution" x)))
+				(gethash "affiliations" search-result) ",")
+			      )
 			(gethash "id" search-result)
 			))
 	      (gethash "results" json-search-res)))
 	  
 	  ;; actual helm query
-	  (helm-oa-sources
-	    `((name . "choose author")
-	       (candidates . oa-candidates)
-	       (action . (lambda (whatever) ;; insert into sqlite db
-			   (mapc (lambda (sel) (skillz-insert-uva-oa-link uva-author-id sel))
-			     (helm-marked-candidates))))))
-	    )
+	  (helm-oa-sources (helm-build-sync-source "pick one"
+			     :candidates oa-candidates
+			     :action (lambda (whatever) ;; insert into sqlite db
+				       (mapc (lambda (sel) (skillz-insert-uva-oa-link uva-author-id sel))
+					 (helm-marked-candidates))))))
     (helm :sources '(helm-oa-sources)))
+   
   )
 
 
-(skillz-author-search "uva2")
-
-
-
-
-
+(skillz-author-search "uva6")
 
 
 
@@ -75,18 +90,10 @@
     
 ;; (sqlite-execute skillz-con "select name from uva_people where uva_author_id = ?" ["uva3"])
 
-(setq skillz-db "~/Dropbox/proj/skilz/skilz.sqlite")
+;; (setq skillz-db "~/Dropbox/proj/skilz/skilz.sqlite")
 
-(setq skillz-con (sqlite-open db-skillz))
+;; (setq skillz-con (sqlite-open db-skillz))
 
-(sqlite-execute con "CREATE TABLE if not exists 'uva_people' ('uva_author_id' string, 'name' string);")
-(sqlite-execute con "CREATE TABLE if not exists 'links_uva_oa' ('uva_author_id' string, 'oa_author_id' string);")
-
-
-  
-;; insert some test values
-;; (sqlite-execute con "insert into uva_people values (?, ?)" '("uva5" "Patrick Brown"))
-
-
-
+;; (sqlite-execute con "CREATE TABLE if not exists 'uva_people' ('uva_author_id' string, 'name' string);")
+;; (sqlite-execute con "CREATE TABLE if not exists 'links_uva_oa' ('uva_author_id' string, 'oa_author_id' string);")
 
